1f2371e234a2f0567e690324a774da82e9c89849
codeyourwaytofreedom <can.kolej@gmail.com>
2023-10-02 19:54:10 +0300

    ....

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,7 +14,7 @@ const NotFound = () => {
                             <Link href={"/"} id={e.home}>Homepage</Link>
                             <Link href={"/"} id={e.consultants}>Consultants</Link>
                             <Link href={"/"} id={e.help}>Help</Link>
-                            <Image width={40} height={350} alt={"404"} src={"/closed.png"} priority/>
+                            <Image width={44} height={73*4} alt={"404"} src={"/closed.png"} priority/>
                         </div>
                         <Image width={480} height={720} alt={"404"} src={"/404.png"} priority/>
                         <h1><span>⚠</span> Page not found!</h1>
