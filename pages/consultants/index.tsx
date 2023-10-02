406a8979cef0c89beba4022415f7faebce1d5ba2
codeyourwaytofreedom <can.kolej@gmail.com>
2023-10-02 20:11:03 +0300

    consultants route added.

---
+++ pages/consultants/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Wrapper from '../../Components/Wrapper'
+import h from "../styles/Homie.module.css";
+
+const Consultants = () => {
+    return ( 
+    <Wrapper title={"Our Team"} login={true}>
+        <h1>Consultants page goes here</h1>
+    </Wrapper>
+     );
+}
+ 
+export default Consultants;
