e5cc1a2b7af637bb1a44db036d38f72b0b284c95
codeyourwaytofreedom <can.kolej@gmail.com>
2023-10-26 12:04:09 +0300

    book appointment section done.
    to be made responsive
    
    Keep going
    
    Remember why you started.

--- pages/dr.tsx
+++ pages/dr.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Wrapper from "../Components/Wrapper";
 import d from "../styles/Dr.module.css";
 import dr from "../public/d.jpeg";
+import Link from "next/link";
 
 
 const DR = () => {
@@ -44,6 +45,42 @@ const DR = () => {
                     </div>
 
 
+                </div>
+
+                <div className={d.dr_book}>
+                    <div className={d.dr_book_kernel}>
+                        <div className={d.dr_book_kernel_left}>
+                            <h2>Book an appointment for free</h2>
+                            <div>Dr. Avdovic partners with Zocdoc to schedule patient appointments</div>
+                            <select>
+                                <optgroup label="Popular Visit Reasons">
+                                    <option value="1" defaultChecked>Illness</option>
+                                    <option value="1">Pediatric Consultation</option>
+                                    <option value="1">Psychiatry Consultation</option>
+                                    <option value="1">Gastroenterology Consultation</option>
+                                    <option value="1">Annual Physical</option>
+                                    <option value="1"></option>
+                                </optgroup>
+                                <optgroup label="All Visit Reasons">
+                                    <option value="1">Abdominal Pain</option>
+                                    <option value="1">Abscess</option>
+                                    <option value="1">Acid Reflux / Heartburn</option>
+                                    <option value="1">Annual Check Up</option>
+                                    <option value="1">Back Pain</option>
+                                    <option value="1">Blood Pressure Testing</option>
+                                </optgroup>
+                            </select>
+                            <div>440 S Riverside Ave, Ground Level, Croton On Hudson, NY 10520</div>
+                            <div><input type="checkbox" defaultChecked/><span> I am a new patient</span></div>
+
+                        </div>
+                        <div className={d.dr_book_kernel_right}>
+                            <div>At this time, the provider has no availability on Zocdoc at this location 
+                                for appointments that meet your search criteria.
+                            </div>
+                            <Link href={"/"}>Notify me of new availability</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Wrapper>
