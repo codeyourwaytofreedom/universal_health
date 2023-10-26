5c4092ec698141b4602f402dfa0ad87a83ddaee7
codeyourwaytofreedom <can.kolej@gmail.com>
2023-10-26 14:57:06 +0300

    Shuffle section created.
    semi responsive
    
    Keep going.
    
    Remember why you started.

--- pages/dr.tsx
+++ pages/dr.tsx
@@ -49,7 +49,17 @@ const DR = () => {
 
                 <div className={d.dr_book}>
                     <div className={d.dr_book_kernel}>
-                        <div className={d.dr_book_kernel_left}>
+                    <svg
+                    viewBox="0 0 44 60"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ width: '44px', height: '60px', position: 'absolute', top: '-40px', right: '-40px' }}
+                    >
+                    <path d="M34.6699 54.3321C34.6699 54.3321 32.3317 53.6226 30.8511 52.5362C29.3705 51.4498 24.6939 50.0307 24.6939 50.0307" stroke="#333333" stroke-width="3" stroke-linecap="round"></path>
+                    <path d="M27.3601 15.3395C27.3601 15.3395 25.0525 17.4668 23.6427 18.7928C22.2329 20.1188 19.3635 23.1692 19.3635 23.1692" stroke="#333333" stroke-width="3" stroke-linecap="round"></path>
+                    <path d="M5.21654 8.61405C5.76208 10.6344 4.11539 13.8472 3.90669 18.0283" stroke="#333333" stroke-width="3" stroke-linecap="round"></path>
+                    <path d="M36.4536 34.8824C36.1363 35.0219 33.5732 35.0729 29.9132 35.337C26.4274 35.5886 24.9323 35.8095 23.504 35.8665" stroke="#333333" stroke-width="3" stroke-linecap="round"></path>
+                    </svg>                        
+                    <div className={d.dr_book_kernel_left}>
                             <h2>Book an appointment for free</h2>
                             <div>Dr. Avdovic partners with Zocdoc to schedule patient appointments</div>
                             <select>
@@ -80,6 +90,64 @@ const DR = () => {
                             </div>
                             <Link href={"/"}>Notify me of new availability</Link>
                         </div>
+                    </div>
+                </div>
+
+                <div className={d.dr_book}>
+                    <div className={d.dr_book_details} id={d.details}>
+                        <div className={d.dr_book_details_tabs}>
+                            <Link href={"#my-anchor"}>About</Link>
+                            <Link href={"#my-anchor"}>Insurances</Link>
+                            <Link href={"#my-anchor"}>Location</Link>
+                            <Link href={"#my-anchor"}>Reviews</Link>
+                            <Link href={"#my-anchor"}>FAQs</Link>
+                        </div>
+                        <div id={d.shuffle}>
+                            <div className={d.dr_book_details_triple} id={d.triple}>
+                                <div className={d.dr_book_details_triple_column}>
+                                    <div className={d.dr_book_details_triple_column_double}>
+                                        <span>&#x2714;</span>
+                                        <div className={d.dr_book_details_triple_column_double_text}>
+                                            <div>Highly recommended</div>
+                                            <div>100% of patients gave this doctor 5 stars</div>
+                                        </div>
+                                    </div>
+                                    <div className={d.dr_book_details_triple_column_double}>
+                                        <span>&#x260E;</span>
+                                        <div className={d.dr_book_details_triple_column_double_text}>
+                                            <div>New patient appointments</div>
+                                            <div>Appointments available for new patients on Zocdoc</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={d.dr_book_details_triple_column}>
+                                    <div className={d.dr_book_details_triple_column_double}>
+                                        <span>&#x23F0;</span>
+                                        <div className={d.dr_book_details_triple_column_double_text}>
+                                            <div>Excellent wait time</div>
+                                            <div>100% of patients waited less than 30 minutes</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id={d.left}>
+                                <h3>Overall Rating</h3>
+                                <h2>5.00</h2>
+                                <div id={d.rating}>&#x2605; &#x2605; &#x2605; &#x2605; &#x2605;</div>
+                                <button>Read 9 reviews</button>
+                            </div>
+                            <div id={d.right}>
+                                <h3>Recent reviews</h3>
+                                <div>Dr and staff are very friendly and professionals..visit for a physical went very smoothly.. We have recommended Dr Advovic to a few people already and they are very happy with him and his services.. Thank you!
+                                    <span>Judy B.February 28, 2023</span>
+                                </div>
+                                <div>Had an in office visit. Got the appointment the same day I called. Dr. Avdovic was prompt, professional and informative. Answered all my questions.
+                                    <span>Less than 1 year ago</span>
+                                </div>
+                                <button>Read 9 reviews</button>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
